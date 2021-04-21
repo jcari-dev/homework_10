039b3a2ba021b288602763fbc35769a0bc2bba4f
jcari-rgb <582119jc@gmail.com>
2021-04-21 11:47:26 -0400

    can add new pokemon!

--- Pokedex/pokedex/server.js
+++ Pokedex/pokedex/server.js
@@ -1,15 +1,30 @@
 const express    = require('express');
-const pokemon = require('./models/Pokemon.js');
 const app        = express();
 const Pokemon    = require('./models/Pokemon.js');
 const port = 3000;
+const methodOverride =require('method-override');
+const pokemon = require('./models/Pokemon.js');
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:false}));
 
+
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
 
 // INDEX
 app.get('/pokemon', (req, res) => {
 res.render('index.ejs', { data: Pokemon });
 });
-
+//
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs')
+});
+app.post('/pokemon', (req, res) =>{
+    pokemon.push(req.body)
+    res.redirect('/pokemon');
+})
 // SHOW
 app.get('/pokemon/:id', (req, res) => {
     console.log(Pokemon[req.params.id], req.params.id)
@@ -17,9 +32,7 @@ res.render('show.ejs', { data: Pokemon[Number(req.params.id)-1] });
 
 });
 
-app.get('/pokemon/new', (req, res) => {
-    res.render('new.ejs')
-});
+
 
 app.listen(port, () => {
     console.log('listening on port', port)
