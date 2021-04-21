13101dea88f83e74168a11bfded11d7e2a913aca
jcari-rgb <582119jc@gmail.com>
2021-04-21 11:00:49 -0400

    show pages now works properly

--- Pokedex/pokedex/server.js
+++ Pokedex/pokedex/server.js
@@ -1,17 +1,24 @@
 const express    = require('express');
+const pokemon = require('./models/Pokemon.js');
 const app        = express();
-const pokemon    = require('./models/pokemon.js');
+const Pokemon    = require('./models/Pokemon.js');
 const port = 3000;
 
 
 // INDEX
-app.get('/', (req, res) => {
-res.render('index.ejs', { data: pokemon });
+app.get('/pokemon', (req, res) => {
+res.render('index.ejs', { data: Pokemon });
 });
 
 // SHOW
-app.get('/:id', (req, res) => {
-res.render('show.ejs', { data: pokemon[req.params.id] });
+app.get('/pokemon/:id', (req, res) => {
+    console.log(Pokemon[req.params.id], req.params.id)
+res.render('show.ejs', { data: Pokemon[Number(req.params.id)-1] });
+
+});
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs')
 });
 
 app.listen(port, () => {
