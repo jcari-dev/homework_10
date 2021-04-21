c72b66ceafea3b40a5399e0c34a35119938ed632
jcari-rgb <582119jc@gmail.com>
2021-04-21 13:04:42 -0400

    can edit pokemons!

--- Pokedex/pokedex/server.js
+++ Pokedex/pokedex/server.js
@@ -17,7 +17,9 @@ app.use((req, res, next) => {
 app.get('/pokemon', (req, res) => {
 res.render('index.ejs', { data: Pokemon });
 });
-//
+
+
+// NEW
 app.get('/pokemon/new', (req, res) => {
     res.render('new.ejs')
 });
@@ -25,15 +27,34 @@ app.post('/pokemon', (req, res) =>{
     pokemon.push(req.body)
     res.redirect('/pokemon');
 })
+// EDIT 
+app.get('/pokemon/:id/edit', (req, res) =>{
+    // console.log(pokemon[req.params.id], req.params.id)
+    res.render('edit.ejs', {
+        data: pokemon[Number(req.params.id)-1],
+        id: req.params.id
+    })
+})
+
+app.put('/pokemon/:id', (req, res) =>{
+    pokemon[Number(req.params.id)-1] = req.body
+    res.redirect('/pokemon');
+})
+
+
+app.post('/pokemon', (req, res) => {
+    data.push(req.body)
+    res.redirect('/pokemon')
+})
 // SHOW
 app.get('/pokemon/:id', (req, res) => {
-    console.log(Pokemon[req.params.id], req.params.id)
+    // console.log(Pokemon[req.params.id], req.params.id)
 res.render('show.ejs', { data: Pokemon[Number(req.params.id)-1] });
 
 });
 
 
-
+// LISTEN
 app.listen(port, () => {
     console.log('listening on port', port)
 });
